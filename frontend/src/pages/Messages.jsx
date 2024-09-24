0f2345c0b089c7ac97887ec91fadebab07ce2d9c
shivenBajpai <shiven.bajpai@gmail.com>
2024-09-24 19:16:34 +0530

    Message groups

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -15,31 +15,51 @@ export default function Messages() {
             id: 2,
             author: "Me",
             timestamp: 1727180924,
-            text: "Great! What about you?"
+            text: "Great! What about you? aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        },
+        {
+            id: 3,
+            author: "Me",
+            timestamp: 1727180925,
+            text: "Im eating Idlis?"
         }
     ];
 
-    const messageGroups = [[]]
+    let messageGroups = [[]]
     let last_message = messages[0].author
-    
-    for (const [idx, message] in messages.entries()) {
-        if (message.author == last_message) messageGroups[-1].push(idx);
+
+    for (const [idx, message] of messages.entries()) {
+        if (message.author == last_message) {
+            messageGroups[messageGroups.length-1].push(idx);
+        }
         else {
             messageGroups.push([idx]);
             last_message = message.author
         }
     }
 
-    const messageElements = messages.map((message) => {
-        if (message.author == User) return <div className="m-2 flex justify-end"><p className="p-2 rounded-lg inline-block bg-blue-400 text-right">{message.author}: {message.text}</p></div>;
-        else return <div className="m-2 flex justify-begin"><p className="p-2 rounded-lg inline-block bg-gray-400 text-right">{message.author}: {message.text}</p></div>
+    const messageElements = messageGroups.map((messageGroup) => {
+        if (messages[messageGroup[0]].author == User) {
+            return <div className="m-2 flex justify-end flex-col">
+                {
+                    messageGroup.map((index) => <p className="m-1 ml-auto p-2 rounded-lg inline-block bg-blue-400 text-right max-w-96 break-words">{messages[index].author}: {messages[index].text}</p>)
+                }
+            </div>;
+        }
+        else {
+            return <div className="m-2 flex justify-begin flex-col">
+                {
+                    messageGroup.map((index) => <p className="p-2 mr-auto rounded-lg inline bg-gray-400 text-left max-w-lg break-words">{messages[index].author}: {messages[index].text}</p>)
+                }
+            </div>;
+        } 
     })
 
     return <>
         <TopNav></TopNav>
         <div className="bg-slate-700 h-screen w-screen flex flex-nowrap flex-row">
-            <div id="contacts_list" className="basis-1/4 border-r-black border-r-2"></div>
-            <div id="message_area" className="grow h-screen flex flex-col">
+            <div id="contacts_list" className="basis-1/4 border-r-black border-r-2 flex-shrink-0"></div>
+            <div id="message_area" className="flex-grow h-screen flex flex-col">
                 {messageElements}
             </div>
         </div>
