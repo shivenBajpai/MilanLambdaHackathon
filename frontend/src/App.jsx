20db3979fc35767e9468d4a190811b04da5fa7cf
shivenBajpai <shiven.bajpai@gmail.com>
2024-09-27 16:54:26 +0530

    Temp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-import Messages from './pages/Messages'
+import MessageWrapper from './pages/Messages'
 import Home from './pages/Home'
 import NoPage from './pages/404'
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, Await } from "react-router-dom";
 import './App.css'
 import TopNav from "./components/Topnav.jsx";
+import { Suspense } from "react"
 
 function Layout() {
   return <div>
@@ -21,7 +22,17 @@ export default function App() {
           <Route index element={<Home />}/>
           <Route path="login" element={<Login />}/>
           <Route path="signup" element={<Signup />}/>
-          <Route path="messages" element={<Messages />}/>
+
+          <Route path="messages" element={
+            <Suspense fallback={<div>Loading...</div>}>
+                <Await
+                    resolve={MessageWrapper()}
+                    errorElement={<div>Could not load user details ?</div>}
+                    children={(element) => {return <div>{element}</div>;}}
+                />
+            </Suspense>
+          }/>
+          
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
